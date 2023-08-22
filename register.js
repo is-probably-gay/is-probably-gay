43784f33cb32fc48e19b0d7218a441aa71d662b0
Charlie <kiwichang2017@gmail.com>
2023-08-22 19:46:53 +0800

    Update register.js

--- register.js
+++ register.js
@@ -30,7 +30,7 @@ if (
   )
 ) {
   return console.log(
-    "not planned,Format invalid! It's usually because you didn't check the agreements, or the domain/record you entered is invalid!"
+    "not planned|\"Format invalid! It's usually because you didn't check the agreements, or the domain/record you entered is invalid!\""
   )
 }
 var flare = require("cloudflare")
@@ -44,20 +44,24 @@ const ipv6 =
 const hostname =
   /^\s*((?=.{1,255}$)[0-9A-Za-z](?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?)*\.?)\s*$/gm
 cf.dnsRecords.browse("2bf779292ec80723b8b7a94bb651ea7d").then((records) => {
-  const filtered = records.result.filter((record) => {
+  const availabilityFilter = records.result.filter((record) => {
     return record.name == array[1][1]
   })
-  if (filtered[0]) {
+  if (availabilityFilter[0]) {
     return console.log(
-      "not planned,This subdomain was taken, please try another subdomain!"
+      "not planned|\"This subdomain was taken, please try another subdomain!\""
     )
   }
+  const countFilter = records.result.filter((record) => {
+    return record.comment == process.env.EVENT_USER_LOGIN
+  })
+  if (countFilter.length==5) {return console.log("not planned|\"You have reached your 5 subdomain limit, please consider deleting some.\"")}
   var type = "invalid"
   if (hostname.test(array[1][1])) type = "CNAME"
   if (ipv4.test(array[1][1])) type = "A"
   if (ipv6.test(array[1][1])) type = "AAAA"
   if (type == "invalid") {
-    return console.log("not planned,The record destination you entered is invalid!")
+    return console.log("not planned|\"The record destination you entered is invalid!\"")
   }
   cf.dnsRecords
     .add("2bf779292ec80723b8b7a94bb651ea7d", {
@@ -73,7 +77,7 @@ cf.dnsRecords.browse("2bf779292ec80723b8b7a94bb651ea7d").then((records) => {
         return console.log(`2,CloudFlare Error:${response.errors[0].message}`)
       }
       console.log(
-        "completed,Your subdomain has been successfully registered! Enjoy it!"
+        "completed|\"Your subdomain has been successfully registered! Enjoy it!\""
       )
     })
 })
